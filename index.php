8701cc1e6d2e930952ae25798c95c16cb532cdfe
Christina L. Cole <christinalcole@gmail.com>
2018-02-10 18:27:29 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+  <meta charset="UTF-8">
+  <title>Microblogging Clone</title>
+</head>
+
+<body>
+  <?php echo("Hello World?  It's always 'Hello, World'..."); ?>
+</body>
+</html>
